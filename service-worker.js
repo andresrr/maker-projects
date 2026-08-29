f1f4349f5f04ffe93900a21cecdc43174d07068d
andresrr <andresrodruiz@gmail.com>
2026-08-30 00:26:01 +0200

    Refresh PWA cache for quick list update

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE='maker-projects-v2';
+const CACHE='maker-projects-v3';
 const ASSETS=["./","./index.html","./manifest.json","./styles.css","./app.js","./projects-1.js","./projects-2.js","./projects-3.js","./projects-4.js","./projects-5.js"];
 self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS))));
 self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k))))));

3cdaac435c5978bccefac15d1ce72f4c3b7004e6
andresrr <andresrodruiz@gmail.com>
2026-08-29 13:43:38 +0200

    Cache modular site assets

--- service-worker.js
+++ service-worker.js
@@ -1,1 +1,5 @@
-const CACHE='maker-projects-v1';const ASSETS=['./','./index.html','./manifest.json'];self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS))));self.addEventListener('fetch',e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))));
+const CACHE='maker-projects-v2';
+const ASSETS=["./","./index.html","./manifest.json","./styles.css","./app.js","./projects-1.js","./projects-2.js","./projects-3.js","./projects-4.js","./projects-5.js"];
+self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS))));
+self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k))))));
+self.addEventListener('fetch',e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))));
